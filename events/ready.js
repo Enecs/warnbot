c01684e39a0e00b12e9289b8465f3efdb7922564
DanPlayz0 <damuffin36@gmail.com>
2021-12-28 03:15:50 +0000

    Set global commands only

--- events/ready.js
+++ events/ready.js
@@ -9,9 +9,7 @@ module.exports = class extends Event {
   async run (client) {
     console.log(`Loaded as ${client.user.tag} (${client.user.id})`);
 
-    // await client.application.commands.set([]);
-    if(client.guilds.cache.get('856626398558027776')) await client.guilds.cache.get('856626398558027776').commands.set(client.commands.map(m => m.commandData));
-    if(client.guilds.cache.get('783178035322159156')) await client.guilds.cache.get('783178035322159156').commands.set(client.commands.map(m => m.commandData));
+    // await client.application.commands.set(client.commands.map(m => m.commandData));
     
     async function setupInit() {
       // Set the game as the "Watching rule breakers"
